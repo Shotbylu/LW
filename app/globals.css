7df7035847536cbb40764df7c19687f8e17d4070
Lungelo Sibisi <lungelosibisi83@gmail.com>
2025-02-13 11:08:35 +0200

    Add files via upload

---
+++ app/globals.css
@@ -0,0 +1,17 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+  font-family: 'Roboto', sans-serif;
+}
+
+.dark-mode {
+  background-color: #0a0e27;
+  color: white;
+}
+
+.light-mode {
+  background-color: white;
+  color: black;
+}
